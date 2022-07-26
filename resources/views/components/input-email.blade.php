aa5847a9d8c1084c44547b03a5d870d6b30da853
grzegorz <grzegorzlis90@gmail.com>
2022-07-26 15:46:22 +0200

    utworzono input-email component

---
+++ resources/views/components/input-email.blade.php
@@ -0,0 +1,3 @@
+<div class="form-group">
+    <input name="{{$name}}" type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+</div>
