807ac69a170042b49fd12f0dc49f0200e0e25580
grzegorz <grzegorzlis90@gmail.com>
2022-08-07 14:46:17 +0200

    dodanie konstruktora oraz metody index do SetController

--- app/Http/Controllers/SetController.php
+++ app/Http/Controllers/SetController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class SetController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+    public function index(Request $request) {
+        $sets = $request->user()->sets()->latest()->paginate(20);
+        
+        return view('admin.sets',[
+            'sets'=>$sets
+        ]);
+    }
 }
