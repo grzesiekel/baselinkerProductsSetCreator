48f8a0891ca137f4d3cb8a6a27ae06b3ae1cdc8d
grzegorz <grzegorzlis90@gmail.com>
2022-11-13 17:59:42 +0100

    add route to searchbar

--- resources/views/components/admin/navbar.blade.php
+++ resources/views/components/admin/navbar.blade.php
@@ -6,13 +6,13 @@
                     </button>
 
                     <!-- Topbar Search -->
-                    <form
+                    <form action="{{route('orders')}}"
                         class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                         <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                            <input type="text" name="id" class="form-control bg-light border-0 small" placeholder="Search for..."
                                 aria-label="Search" aria-describedby="basic-addon2">
                             <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
+                                <button class="btn btn-primary" type="submit">
                                     <i class="fas fa-search fa-sm"></i>
                                 </button>
                             </div>
@@ -31,13 +31,13 @@
                             <!-- Dropdown - Messages -->
                             <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
                                 aria-labelledby="searchDropdown">
-                                <form class="form-inline mr-auto w-100 navbar-search">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-0 small"
+                                <form method="GET" action="{{route('orders')}}" class="form-inline mr-auto w-100 navbar-search">
+                                   <div class="input-group">
+                                        <input type="text" name="id" class="form-control bg-light border-0 small"
                                             placeholder="Search for..." aria-label="Search"
                                             aria-describedby="basic-addon2">
                                         <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button">
+                                            <button class="btn btn-primary" type="submit">
                                                 <i class="fas fa-search fa-sm"></i>
                                             </button>
                                         </div>
